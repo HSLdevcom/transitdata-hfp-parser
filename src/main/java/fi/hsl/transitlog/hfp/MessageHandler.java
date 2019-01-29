7cd4037c29995ad8d2736dbc488629e5e7852276
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-01-29 14:18:34 +0200

    Parse Hfp data

--- src/main/java/fi/hsl/transitlog/hfp/MessageHandler.java
+++ src/main/java/fi/hsl/transitlog/hfp/MessageHandler.java
@@ -1,6 +1,12 @@
 package fi.hsl.transitlog.hfp;
 
 import com.typesafe.config.Config;
+import fi.hsl.common.hfp.HfpData;
+import fi.hsl.common.hfp.HfpParser;
+import fi.hsl.common.hfp.HfpPayload;
+import fi.hsl.common.hfp.HfpTopic;
+import fi.hsl.common.hfp.proto.Hfp;
+import fi.hsl.common.mqtt.proto.Mqtt;
 import fi.hsl.common.pulsar.IMessageHandler;
 import fi.hsl.common.pulsar.PulsarApplicationContext;
 import fi.hsl.common.transitdata.TransitdataProperties;
@@ -18,6 +24,8 @@ public class MessageHandler implements IMessageHandler {
     private Consumer<byte[]> consumer;
     private Producer<byte[]> producer;
     private Config config;
+
+    private final HfpParser parser = HfpParser.newInstance();
 
     public MessageHandler(PulsarApplicationContext context) {
         consumer = context.getConsumer();
@@ -41,32 +49,63 @@ public class MessageHandler implements IMessageHandler {
     public void handleMessage(Message received) throws Exception {
         try {
             Optional<ProtobufSchema> maybeSchema = parseProtobufSchema(received);
-            maybeSchema.ifPresent(schema -> {
+            if (maybeSchema.isPresent() && maybeSchema.get() == ProtobufSchema.MqttRawMessage) {
+                final long timestamp = received.getEventTime();
+                byte[] data = received.getData();
 
-            });
-
-            consumer.acknowledgeAsync(received)
-                    .exceptionally(throwable -> {
-                        log.error("Failed to ack Pulsar message", throwable);
-                        return null;
-                    })
-                    .thenRun(() -> {});
+                Hfp.Data converted = parseData(data, timestamp);
+                sendPulsarMessage(received.getMessageId(), converted, timestamp);
+            }
+            else {
+                log.warn("Received unexpected schema, ignoring.");
+                ack(received.getMessageId()); //Ack so we don't receive it again
+            }
         }
         catch (Exception e) {
             log.error("Exception while handling message", e);
         }
     }
 
-    private void sendPulsarMessage() {
-        /*GtfsRealtime.FeedMessage feedMessage = GtfsRtFactory.newFeedMessage(dvjId, tripUpdate, tripUpdate.getTimestamp());
+    private void ack(MessageId received) {
+        consumer.acknowledgeAsync(received)
+                .exceptionally(throwable -> {
+                    log.error("Failed to ack Pulsar message", throwable);
+                    return null;
+                })
+                .thenRun(() -> {});
+    }
+
+    Hfp.Data parseData(byte[] data, long timestamp) throws Exception {
+        final Mqtt.RawMessage raw = Mqtt.RawMessage.parseFrom(data);
+        final String rawTopic = raw.getTopic();
+        final byte[] rawPayload = raw.getPayload().toByteArray();
+
+        final HfpPayload payload = parser.parse(rawPayload);
+        final Optional<HfpTopic> maybeTopic = HfpParser.parseTopic(rawTopic, timestamp);
+
+        //TODO
+        return null;
+    }
+
+    private void sendPulsarMessage(MessageId received, Hfp.Data hfp, long timestamp) {
+
         producer.newMessage()
-                .key(dvjId)
-                .eventTime(pulsarEventTimestamp)
-                .property(TransitdataProperties.KEY_PROTOBUF_SCHEMA, TransitdataProperties.ProtobufSchema.GTFS_TripUpdate.toString())
-                .value(feedMessage.toByteArray())
+                //.key(dvjId) //TODO think about this
+                .eventTime(timestamp)
+                .property(TransitdataProperties.KEY_PROTOBUF_SCHEMA, ProtobufSchema.HfpData.toString())
+                .value(hfp.toByteArray())
                 .sendAsync()
-                .thenRun(() -> log.debug("Sending TripUpdate for dvjId {} with {} StopTimeUpdates and status {}",
-                        dvjId, tripUpdate.getStopTimeUpdateCount(), tripUpdate.getTrip().getScheduleRelationship()));*/
+                .whenComplete((MessageId id, Throwable t) -> {
+                    if (t != null) {
+                        log.error("Failed to send Pulsar message", t);
+                        //Should we abort?
+                    }
+                    else {
+                        //Does this become a bottleneck? Does pulsar send more messages before we ack the previous one?
+                        //If yes we need to get rid of this
+                        ack(received);
+                    }
+                });
 
     }
 }
