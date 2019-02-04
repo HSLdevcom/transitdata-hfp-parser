0d82fe4646704b78e305801f71b9b9dd6cacb232
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-02-04 12:27:26 +0200

    Use shared code from commons

--- src/main/java/fi/hsl/transitlog/hfp/MessageHandler.java
+++ src/main/java/fi/hsl/transitlog/hfp/MessageHandler.java
@@ -9,6 +9,7 @@ import fi.hsl.common.pulsar.IMessageHandler;
 import fi.hsl.common.pulsar.PulsarApplicationContext;
 import fi.hsl.common.transitdata.TransitdataProperties;
 import fi.hsl.common.transitdata.TransitdataProperties.*;
+import fi.hsl.common.transitdata.TransitdataSchema;
 import org.apache.pulsar.client.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,23 +32,11 @@ public class MessageHandler implements IMessageHandler {
         this.config = context.getConfig();
     }
 
-    private Optional<ProtobufSchema> parseProtobufSchema(Message received) {
-        try {
-            String schemaType = received.getProperty(TransitdataProperties.KEY_PROTOBUF_SCHEMA);
-            log.debug("Received message with schema type " + schemaType);
-            ProtobufSchema schema = ProtobufSchema.fromString(schemaType);
-            return Optional.of(schema);
-        }
-        catch (Exception e) {
-            log.error("Failed to parse protobuf schema", e);
-            return Optional.empty();
-        }
-    }
-
     public void handleMessage(Message received) throws Exception {
         try {
-            Optional<ProtobufSchema> maybeSchema = parseProtobufSchema(received);
-            if (maybeSchema.isPresent() && maybeSchema.get() == ProtobufSchema.MqttRawMessage) {
+            Optional<TransitdataSchema> maybeSchema = TransitdataSchema.parseFromPulsarMessage(received);
+
+            if (maybeSchema.isPresent() && maybeSchema.get().schema == ProtobufSchema.MqttRawMessage) {
                 final long timestamp = received.getEventTime();
                 byte[] data = received.getData();
 
