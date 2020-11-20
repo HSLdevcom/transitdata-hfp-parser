db4e5307fefc4ae22543755c6beb264abb8a90ef
Pierre Leonard <pierre.leonard@digia.com>
2020-11-20 17:09:41 +0200

    Parsing passenger count json and turning it into protobuf

--- src/main/java/fi/hsl/transitdata/hfp/MessageHandler.java
+++ src/main/java/fi/hsl/transitdata/hfp/MessageHandler.java
@@ -1,21 +1,26 @@
 package fi.hsl.transitdata.hfp;
 
-import com.google.protobuf.InvalidProtocolBufferException;
+
+import com.google.protobuf.GeneratedMessageV3;
 import fi.hsl.common.hfp.HfpJson;
 import fi.hsl.common.hfp.HfpParser;
 import fi.hsl.common.hfp.proto.Hfp;
 import fi.hsl.common.mqtt.proto.Mqtt;
+import fi.hsl.common.passengercount.PassengerCountParser;
+import fi.hsl.common.passengercount.json.APCJson;
+import fi.hsl.common.passengercount.proto.PassengerCount;
 import fi.hsl.common.pulsar.IMessageHandler;
 import fi.hsl.common.pulsar.PulsarApplicationContext;
 import fi.hsl.common.transitdata.TransitdataProperties;
-import fi.hsl.common.transitdata.TransitdataProperties.*;
 import fi.hsl.common.transitdata.TransitdataSchema;
-import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-
 
 public class MessageHandler implements IMessageHandler {
     private static final Logger log = LoggerFactory.getLogger(MessageHandler.class);
@@ -25,7 +30,8 @@ public class MessageHandler implements IMessageHandler {
     private final Producer<byte[]> passengerCountProducer;
 
     private static final String PASSENGER_COUNT_MQTT_TOPIC = "";
-    private final HfpParser parser = HfpParser.newInstance();
+    private final HfpParser hfpParser = HfpParser.newInstance();
+    private final PassengerCountParser passengerCountParser = PassengerCountParser.newInstance();
 
     public MessageHandler(PulsarApplicationContext context) {
         consumer = context.getConsumer();
@@ -35,15 +41,16 @@ public class MessageHandler implements IMessageHandler {
 
     public void handleMessage(Message received) {
         try {
-            if (TransitdataSchema.hasProtobufSchema(received, ProtobufSchema.MqttRawMessage)) {
+            if (TransitdataSchema.hasProtobufSchema(received, TransitdataProperties.ProtobufSchema.MqttRawMessage)) {
                 final long timestamp = received.getEventTime();
+                byte[] data = received.getData();
                 if(PASSENGER_COUNT_MQTT_TOPIC.equals(received.getTopicName())){
-                    sendPassengerCountPulsarMessage(received);
+                    PassengerCount.Data converted = parsePassengerCountData(data);
+                    sendPulsarMessage(received.getMessageId(), converted, timestamp, TransitdataProperties.ProtobufSchema.PassengerCount.toString(), passengerCountProducer);
                 }
                 else{
-                    byte[] data = received.getData();
-                    Hfp.Data converted = parseData(data, timestamp);
-                    sendHfpPulsarMessage(received.getMessageId(), converted, timestamp);
+                    Hfp.Data converted = parseHfpData(data, timestamp);
+                    sendPulsarMessage(received.getMessageId(), converted, timestamp,  TransitdataProperties.ProtobufSchema.HfpData.toString(), hfpProducer);
                 }
 
             }
@@ -69,12 +76,27 @@ public class MessageHandler implements IMessageHandler {
                 .thenRun(() -> {});
     }
 
-    Hfp.Data parseData(byte[] data, long timestamp) throws IOException, HfpParser.InvalidHfpPayloadException, HfpParser.InvalidHfpTopicException {
+
+    PassengerCount.Data parsePassengerCountData(byte[] data) throws IOException, PassengerCountParser.InvalidAPCPayloadException {
+        final Mqtt.RawMessage raw = Mqtt.RawMessage.parseFrom(data);
+        final String rawTopic = raw.getTopic();
+        final byte[] rawPayload = raw.getPayload().toByteArray();
+        final APCJson apcJson = passengerCountParser.parseJson(rawPayload);
+        PassengerCount.Payload payload = PassengerCountParser.newInstance().parsePayload(apcJson);
+
+        PassengerCount.Data.Builder builder = PassengerCount.Data.newBuilder();
+        builder.setPayload(payload);
+        builder.setTopic(rawTopic);// Maybe we need additional logic for this
+
+        return builder.build();
+    }
+
+    Hfp.Data parseHfpData(byte[] data, long timestamp) throws IOException, HfpParser.InvalidHfpPayloadException, HfpParser.InvalidHfpTopicException {
         final Mqtt.RawMessage raw = Mqtt.RawMessage.parseFrom(data);
         final String rawTopic = raw.getTopic();
         final byte[] rawPayload = raw.getPayload().toByteArray();
 
-        final HfpJson jsonPayload = parser.parseJson(rawPayload);
+        final HfpJson jsonPayload = hfpParser.parseJson(rawPayload);
         Hfp.Payload payload = HfpParser.parsePayload(jsonPayload);
         Hfp.Topic topic = HfpParser.parseTopic(rawTopic, timestamp);
 
@@ -85,33 +107,12 @@ public class MessageHandler implements IMessageHandler {
         return builder.build();
     }
 
-
-    private void sendPassengerCountPulsarMessage(Message message) {
-        passengerCountProducer.newMessage()
-            //.key(message.getKey())
-            .eventTime(message.getEventTime())
-            .properties(message.getProperties())
-            .value(message.getData())
-            .sendAsync()
-            .whenComplete((o, t) -> {
-                if (t != null) {
-                    log.error("Failed to send Pulsar message", t);
-                    //Should we abort?
-                }
-                else {
-                    //Does this become a bottleneck? Does pulsar send more messages before we ack the previous one?
-                    //If yes we need to get rid of this
-                    ack(message.getMessageId());
-                }
-            });
-    }
-
-    private void sendHfpPulsarMessage(MessageId received, Hfp.Data hfp, long timestamp) {
-        hfpProducer.newMessage()
+    private void sendPulsarMessage(MessageId received, GeneratedMessageV3 message, long timestamp, String protobufSchema, Producer<byte[]> producer) {
+        producer.newMessage()
             //.key(dvjId) //TODO think about this
             .eventTime(timestamp)
-            .property(TransitdataProperties.KEY_PROTOBUF_SCHEMA, ProtobufSchema.HfpData.toString())
-            .value(hfp.toByteArray())
+            .property(TransitdataProperties.KEY_PROTOBUF_SCHEMA, protobufSchema)
+            .value(message.toByteArray())
             .sendAsync()
             .whenComplete((MessageId id, Throwable t) -> {
                 if (t != null) {
