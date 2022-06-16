4c580e48554c30ef8f39829b1bcb1cbb2b7a7adc
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-06-16 10:22:53 +0300

    Add basic healthcheck to make sure that data is being processed

--- src/main/java/fi/hsl/transitdata/hfp/Main.java
+++ src/main/java/fi/hsl/transitdata/hfp/Main.java
@@ -2,9 +2,12 @@ package fi.hsl.transitdata.hfp;
 
 import com.typesafe.config.Config;
 import fi.hsl.common.config.ConfigParser;
+import fi.hsl.common.health.HealthServer;
 import fi.hsl.common.pulsar.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -17,6 +20,22 @@ public class Main {
             PulsarApplicationContext context = app.getContext();
 
             MessageHandler router = new MessageHandler(context, config.getString("application.messageType"));
+
+            final Duration unhealthyIfNoAck = context.getConfig().getDuration("application.unhealthyIfNoAck");
+
+            HealthServer healthServer = context.getHealthServer();
+            if (healthServer != null) {
+                healthServer.addCheck(() -> {
+                    final Duration timeSinceAck = Duration.ofNanos(System.nanoTime() - router.getLastAckTime());
+
+                    final boolean healthy = timeSinceAck.compareTo(unhealthyIfNoAck) >= 0;
+                    if (!healthy) {
+                        log.warn("Service unhealthy, last message acknowledged {} seconds ago", timeSinceAck.getSeconds());
+                    }
+
+                    return healthy;
+                });
+            }
 
             log.info("Start handling the messages");
             app.launchWithHandler(router);
